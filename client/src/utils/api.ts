e4f20b16dc0c900aa60df0b8d025abb6bbc5960c
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-10-16 02:30:10 +0530

    make req to upsert

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,5 +1,16 @@
-export const postData = (updateTasks: Task[]) => {
+export const postData = async (updateTasks: Task[]) => {
     console.log("Tasks to update\n", updateTasks)
+    const reqBody = { tasks: updateTasks }
+    let res = await fetch("http://localhost:3000/task", {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(reqBody)
+    })
+    res = await res.json()
+    console.log(res)
+
 }
 
 export const postAndDeleteData = (updateTasks: Task[], deleteTasks: Task[]) => {
