5fb72a41e5c068af8e9bf0f68704d55c2ca6ce99
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-10-16 01:02:38 +0530

    remove redundant code

--- client/src/hooks/useBoard.ts
+++ client/src/hooks/useBoard.ts
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react"
 
-// utils
-import { sortTaskAscending } from "../utils/taskUtils";
-
 const useBoard = (id: number, boardDispatch: React.Dispatch<BoardAction>) => {
 
     const [loading, setLoading] = useState<boolean>(true);
@@ -17,20 +14,6 @@ const useBoard = (id: number, boardDispatch: React.Dispatch<BoardAction>) => {
                     throw new Error("Failed to fetch board data")
                 }
                 const board = await res.json() as Board
-
-                const statuses = Array.from(new Set(board.tasks.map(task => task.status)));
-                const sortedTasks: TaskLayout = { backlog: [], todo: [], in_progress: [], completed: [] }
-
-                for (let status of statuses) {
-                    // create status key if it doesn't exist
-                    if (!sortedTasks[status]) {
-                        sortedTasks[status] = [];
-                    }
-                    sortedTasks[status].push(
-                        ...board.tasks.filter(t => t.status === status)
-                            .sort(sortTaskAscending)
-                    )
-                }
                 boardDispatch({ type: "SET_BOARD", payload: board })
             } catch (err) {
                 const error = err as Error
