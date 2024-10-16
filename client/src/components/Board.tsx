0181123720aba37c7807b8fb982965d65f1e9d29
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-10-16 22:15:00 +0530

    Board Form to add list

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -7,19 +7,25 @@ import DraggedTaskContext from "../context/draggedTask"
 // Components
 import List from "../components/List"
 
-const Board: React.FC<{
+interface Props {
     board: BoardState
-}> = ({ board }) => {
+}
+
+const Board: React.FC<Props> = ({ board }) => {
 
     // State
     const [draggedTask, setDraggedTask] = useState<Task>({ id: 0, name: "", order: 0, status: "backlog", board: 0 });
+    const [addingStatusList, setAddingStatusList] = useState<boolean>(false)
     // Context
     const boardDispatch = useContext(BoardDispatchContext)
 
     // Function to add new status list
-    const addNewStatusList = (e: React.MouseEvent) => {
+    const addNewStatusList = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        boardDispatch!({ type: "ADD_STATUS", payload: { status: "Foo" } })
+        const target = e.target as HTMLFormElement
+        const value = target.list.value
+        boardDispatch!({ type: "ADD_STATUS", payload: { status: value } })
+        setAddingStatusList(false)
     }
 
     return (
@@ -27,17 +33,28 @@ const Board: React.FC<{
             <h1 className="pl-12 bg-white opacity-50 text-4xl">{board.name}</h1>
             <div className="px-10 py-4 flex items-start board">
                 <DraggedTaskContext.Provider value={{ draggedTask, setDraggedTask }}>
+                    {Object.keys(board.tasks).map((status, key) =>
+                        <List
+                            key={key}
+                            className="mr-10"
+                            title={status}
+                            board={board} />
+                    )}
                     {
-                        <>
-                            {Object.keys(board.tasks).map((status, key) =>
-                                <List
-                                    key={key}
-                                    className="mr-10"
-                                    title={status}
-                                    board={board} />
-                            )}
-                            <button onClick={addNewStatusList}>Create New List</button>
-                        </>
+                        addingStatusList ?
+                            <form onSubmit={addNewStatusList} className="shrink-0 w-64 px-4 py-5 bg-black text-gray-300 rounded-xl h-fitk mb-3">
+                                <input
+                                    className="px-3 pt-1.5 pb-6 mb-3 w-full bg-gray-800 rounded-lg"
+                                    type="text"
+                                    name="list"
+                                    placeholder="Enter a title for this List"
+                                />
+                                <div className="flex">
+                                    <button type="submit" className="px-3 py-1 mr-4 bg-sky-800 text-white rounded-sm">Add</button>
+                                    <button onClick={() => setAddingStatusList(false)}>x</button>
+                                </div>
+                            </form> :
+                            <button onClick={(_) => setAddingStatusList(true)}>Create New List</button>
                     }
                 </DraggedTaskContext.Provider >
             </div>
