f3ce681799a10d58880ad46ba9d91dd711311352
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-10-17 23:51:47 +0530

    move create list button

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -73,6 +73,12 @@ const Board: React.FC<Props> = ({ board }) => {
                 </h1>
             }
 
+            {
+                addingStatusList ?
+                    <PrimaryBtn className="ml-10 mt-5" onClick={(_) => setAddingStatusList(false)}>Cancel</PrimaryBtn> :
+                    <PrimaryBtn className="ml-10 mt-5" onClick={(_) => setAddingStatusList(true)}>Create New List</PrimaryBtn>
+            }
+
             <div className="px-10 py-4 flex items-start board overflow-x-scroll no-scrollbar">
                 <DraggedTaskContext.Provider value={{ draggedTask, setDraggedTask }}>
                     {Object.keys(board.tasks).map((status, key) =>
@@ -83,20 +89,19 @@ const Board: React.FC<Props> = ({ board }) => {
                             board={board} />
                     )}
                     {
-                        addingStatusList ?
-                            <form onSubmit={addNewStatusList} className="shrink-0 w-64 px-4 py-5 bg-black text-gray-300 rounded-xl h-fitk mb-3">
-                                <input
-                                    className="px-3 pt-1.5 pb-6 mb-3 w-full bg-gray-800 rounded-lg"
-                                    type="text"
-                                    name="list"
-                                    placeholder="Enter a title for this List"
-                                />
-                                <div className="flex">
-                                    <PrimaryBtn type="submit" className="mr-4">Add</PrimaryBtn>
-                                    <button onClick={() => setAddingStatusList(false)}>x</button>
-                                </div>
-                            </form> :
-                            <PrimaryBtn onClick={(_) => setAddingStatusList(true)}>Create New List</PrimaryBtn>
+                        addingStatusList &&
+                        <form onSubmit={addNewStatusList} className="shrink-0 w-64 px-4 py-5 bg-black text-gray-300 rounded-xl h-fitk mb-3">
+                            <input
+                                className="px-3 pt-1.5 pb-6 mb-3 w-full bg-gray-800 rounded-lg"
+                                type="text"
+                                name="list"
+                                placeholder="Enter a title for this List"
+                            />
+                            <div className="flex">
+                                <PrimaryBtn type="submit" className="mr-4">Add</PrimaryBtn>
+                                <button onClick={() => setAddingStatusList(false)}>x</button>
+                            </div>
+                        </form>
                     }
                 </DraggedTaskContext.Provider >
             </div>
